4d27fcf81c399daa70f335e025f87491feaa9499
yamirui <yamirui.git@gmail.com>
2020-09-20 12:26:00 +0300

    Initial commit

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(name='zup',
+      version='0.0.0',
+      description='Zig compiler multiplexer',
+      classifiers = [
+          'Programming Language :: Python :: 3.8',
+          'License :: OSI Approved :: zlib/libpng License',
+          'Operating System :: OS Independent',
+          'Topic :: System :: Installation/Setup',
+          'Development Status :: 1 - Planning'
+      ],
+      keywords='zig',
+      url='https://github.com/yamirui/zup',
+      author='yamirui',
+      author_email='yamirui.git@gmail.com',
+      license='zlib',
+      packages=['zup'],
+      entry_points = {
+        'console_scripts': ['zup=zup.main:main'],
+      },
+      include_package_data=True,
+      zip_safe=True,
+      python_requires='>=3.8')
---
+++ zup/main.py
@@ -0,0 +1,4 @@
+import zup
+
+def main():
+    zup.run()
